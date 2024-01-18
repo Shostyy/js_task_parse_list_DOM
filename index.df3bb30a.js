c25376ceb787359e9662fea48a0d00c0c8f03163
Shostyy <anatoliy.shostal@gmail.com>
2024-01-18 19:58:22 +0200

    make build

---
+++ index.df3bb30a.js
@@ -0,0 +1,3 @@
+var a,r;r=Array.from((a=document.querySelector("ul")).children).sort(function(a,r){var e=parseFloat(a.dataset.salary.replace("$","").replaceAll(",",""));return parseFloat(r.dataset.salary.replace("$","").replaceAll(",",""))-e}),a.innerHTML="",r.forEach(function(r){a.appendChild(r)});//# sourceMappingURL=index.df3bb30a.js.map
+
+//# sourceMappingURL=index.df3bb30a.js.map
